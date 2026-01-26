0dad3275fb4b22695015bd244dcc5979b107bd90
Rashed811 <rashedsarker811@gmail.com>
2026-01-26 14:45:53 +0600

    first python code

---
+++ hello.py
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+     <head>
+        <title> html id fun</title>
+     </head>
+
+     <body> 
+        <h1 title="this is a title">Title</h1>
+<p> larning html new</p>
+<p>html is Not case Sensitive </p>
+     </body>
+</html>
